72d835bcf0e1e053c8a9d291637cb3f2582708a7
KevTheFoo <kevthefoo@gmail.com>
2024-09-08 20:50:30 +1000

    Refactor code formatting in page.js and package.json

--- app/page.js
+++ app/page.js
@@ -1,17 +1,17 @@
-import Landing from "./Home/Landing/Landing"
+import Landing from "./Home/Landing/Landing";
 import Introduction from "./Home/Introduction/Introduction";
 import Feature from "./Home/Feature/Feature";
 import Tools from "./Home/Tools/Tools";
 import Safe from "./Home/Safe/Safe";
 
 export default function Home() {
-  return (
-    <>
-      <Landing />
-      <Introduction />
-      <Feature />
-      <Tools />
-      <Safe />
-    </>
-  );
+    return (
+        <>
+            <Landing />
+            <Introduction />
+            <Feature />
+            <Tools />
+            <Safe />
+        </>
+    );
 }
