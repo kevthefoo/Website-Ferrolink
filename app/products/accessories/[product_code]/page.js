42c17dfd92a3b7a29097183de19d50c28fe6264c
KevTheFoo <kevthefoo@gmail.com>
2024-09-08 22:44:34 +1000

    Refactor code formatting and add product details to singleProductPage component

--- app/products/accessories/[product_code]/page.js
+++ app/products/accessories/[product_code]/page.js
@@ -1,3 +1,22 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export default function singleProductPage({ params }) {
-    return <section className="">{params.product_code}</section>;
+    const { product_code } = params;
+    const imagePath = require(`../../../../assets/products/Hammer/${product_code}.jpg`);
+
+    return (
+        <section className="flex flex-col justify-start pt-16 items-center border-2 border-red-500">
+            <h1 className="text-center">{product_code}</h1>
+            <div className="flex w-full justify-around items-center border-2 border-blue-400">
+                <Image src={imagePath} width={300} height={300} alt="xx" />
+                <div>
+                    <h2>Weight</h2>
+                    <p>100kg</p>
+                    <h2>Dimension</h2>
+                    <p>12x12x12</p>
+                </div>
+            </div>
+        </section>
+    );
 }
